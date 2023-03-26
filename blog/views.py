08be5c6130db1305c849e2ab5ee6197e71bf3267
Fadoua <fadelhannachi@gmail.com>
2023-03-26 13:01:33 +0200

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Fadoua',
+        'title': "Blog post 1",
+        'content': 'First post content',
+        'date_posted': 'March 26, 2023'
+    },
+    {
+        'author': 'Fad',
+        'title': "Blog post 2",
+        'content': 'Second post content',
+        'date_posted': 'April 27, 2023'
+    },
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    content={
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', content)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
