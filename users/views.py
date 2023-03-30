a97ba78e5996c6178f69f1e2d2a10992b32cdb7d
Fadoua <fadelhannachi@gmail.com>
2023-03-30 11:34:09 +0200

    typo correction

--- users/views.py
+++ users/views.py
@@ -3,14 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_date.get('username')
+            username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
-
+            return redirect('blog-home') #nous ramene au menu principal
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
